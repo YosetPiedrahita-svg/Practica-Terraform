1deea8c51feb45c8bb6aaa6c8af845c8db11ff3e
YosetPiedrahita-svg <yosetalfonsopiedrahita@gmail.com>
2026-09-09 19:35:40 -0500

    feat: agregar main.tf con sufijo

---
+++ main.tf
@@ -0,0 +1,55 @@
+# Proveedor
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 6.0"
+    }
+  }
+}
+
+# Parámetros Globales de Conexión
+provider "google" {
+  project = "project-81bad290-05ae-4c0f-9f6"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
+
+# Creación de la Regla de Cortafuegos
+resource "google_compute_firewall" "permitir_http" {
+  name    = "permitir-http-yoset"
+  network = "default"
+
+  # Puertos
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  # Filtro
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["servidor-web"]
+}
+
+# Creación de la máquina virtual
+resource "google_compute_instance" "web" {
+  name         = "web-tf-yoset"
+  machine_type = "e2-micro"
+  tags         = ["servidor-web"]
+
+  # Disco duro y sistema operativo
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+
+  # Interfaz de red e IP asignada
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+
+  # Ejecutar el script al iniciar 
+  metadata_startup_script = file("startup.sh")
+}
